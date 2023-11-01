5e95f7707e2efc689c7dc7a8a855a5b441558fae
Félix Faisant <xcodexif@xif.fr>
2023-11-01 23:17:23 +0100

    typos, commentaires

--- TD2/methode_newton_autozero.cpp
+++ TD2/methode_newton_autozero.cpp
@@ -7,8 +7,18 @@
 
 // note : on utilise ici le type std::complex de la bibliothèque standard, qui devrait fonctionner exactement comme votre classe complexe
 using complexe = std::complex<double>;
+// déclaration du type fonction complexe -> complexe
 using fonc_complexe_t = std::function<complexe(complexe)>;
 
+/*
+  Algorithme de la méthode de Newton, prenant une fonction `f` et sa dérivée `f_deriv`,
+  et cherchant un zéro de `f` en partant de `z`. On dit que l'on a obtenu un zéro `z_star` lorsque
+  |f(z_star)| < epsilon, et `methode_newton` renvoit la paire (z_star,n_it), où `n_it` est le
+  nombre d'itérations nécessaires pour l'atteindre. Si un zéro n'est pas atteint en `max_it`
+  itérations, on déclare qu'il n'y a pas eu convergence en renvoyant `n_it=-1`.
+  Optionellement, on peut donner un facteur d'amortissement `h`, ainsi qu'une fonction callback `cb_iter`,
+  appelée à chaque itération. Si `debug=true`, un message est affiché à chaque itération.
+*/
 std::tuple<complexe,int> methode_newton (
   fonc_complexe_t f, fonc_complexe_t f_deriv,
   complexe z,
@@ -16,12 +26,12 @@ std::tuple<complexe,int> methode_newton (
   bool debug = false, std::function<void(complexe)> cb_iter = std::function<void(complexe)>()
 ) {
   int n = 0;
-  complexe fz = f(z);
+  complexe fz = f(z); // valeur courant de la fonction
+  // On itère tant que |f(z)| > ɛ. Pour ne pas avoir à calculer une coûteuse racine carrée, on regarde plutôt |f(z)|^2 < espilon^2.
   epsilon *= epsilon;
-  // on regarde plutôt |f(z)|^2 < espilon^2
   while (std::norm(fz) > epsilon) {
     if (n >= max_it)
-      return { z, -1 };
+      return { z, -1 }; // pas de convergence
     z -= h * fz / f_deriv(z);
     n += 1;
     fz = f(z);
@@ -34,36 +44,47 @@ std::tuple<complexe,int> methode_newton (
 }
 
 int main () {
+  // Note fonction et sa dérivée
   fonc_complexe_t f = [] (complexe z) { auto z2 = z*z; return z2*z2 - 2.*z2 - complexe(1,0); };
   fonc_complexe_t f_deriv = [] (complexe z) { return 4.*z*z*z - 4.*z; };
+  // Stockage des zéros que l'on trouvera
   std::vector<complexe> zeros;
 
+  /******* Scan de z_0 *******/
+
+  // Définition de la grille
   int N = 1000;
   double a = 2, da = a/N;
 
+  // Fichier stockant le numéro du zéro obtenu
   std::ofstream fichier_bassins_converg ("newton_bassins_converg.txt");
+  // Fichier stockant le nombre d'itérations nécessaires
   std::ofstream fichier_niter ("newton_niter.txt");
 
   for (int i = -N; i <= N; i++) {
     std::cout << i << std::endl;
     for (int j = -N; j <= N; j++) {
 
+      // Méthode de Newton partant de z_0
       complexe z_0 = {i*da, j*da};
-      auto [z_zero, nit] = methode_newton(f, f_deriv, z_0, 200, a*1e-15, 1, false);
+      auto [z_zero, nit] = methode_newton(f, f_deriv, z_0, 200, a*1e-15);
 
-      // détermination du numéro du zéro
+      // Détermination du numéro du zéro (si on a convergé)
       int num_zero = -1;
       if (nit >= 0) {
+        // Recherche parmi les zéros déjà obtenus
         for (int k = 0; k < zeros.size(); k++) {
           if (std::norm(z_zero-zeros[k]) < 1e-10)
             num_zero = k;
         }
+        // Ajout de l'éventuel nouveau zéro
         if (num_zero == -1) {
           zeros.push_back(z_zero);
           num_zero = zeros.size() - 1;
         }
       }
 
+      // Écriture dans les fichiers
       fichier_bassins_converg << num_zero << " ";
       fichier_niter << nit << " ";
     }
@@ -71,6 +92,7 @@ int main () {
     fichier_niter << "\n";
   }
 
+  // Affichage de tous les zéros obtenus
   std::cout << "Zéros :" << std::endl;
   for (complexe z : zeros) {
     std::cout << z << std::endl;
--- TD2/methode_newton_manualzero.cpp
+++ TD2/methode_newton_manualzero.cpp
@@ -7,22 +7,32 @@
 
 // note : on utilise ici le type std::complex de la bibliothèque standard, qui devrait fonctionner exactement comme votre classe complexe
 using complexe = std::complex<double>;
+// déclaration du type fonction complexe -> complexe
 using fonc_complexe_t = std::function<complexe(complexe)>;
 
+/*
+  Algorithme de la méthode de Newton, prenant une fonction `f` et sa dérivée `f_deriv`,
+  et cherchant un zéro de `f` en partant de `z`. On dit que l'on a obtenu un zéro `z_star` lorsque
+  |f(z_star)| < epsilon, et `methode_newton` renvoit la paire (z_star,n_it), où `n_it` est le
+  nombre d'itérations nécessaires pour l'atteindre. Si un zéro n'est pas atteint en `max_it`
+  itérations, on déclare qu'il n'y a pas eu convergence en renvoyant `n_it=-1`.
+  Optionellement, on peut donner une fonction callback `cb_iter`,
+  appelée à chaque itération. Si `debug=true`, un message est affiché à chaque itération.
+*/
 std::tuple<complexe,int> methode_newton (
   fonc_complexe_t f, fonc_complexe_t f_deriv,
   complexe z,
-  unsigned int max_it, double epsilon, double h = 1.,
+  unsigned int max_it, double epsilon,
   bool debug = false, std::function<void(complexe)> cb_iter = std::function<void(complexe)>()
 ) {
   int n = 0;
-  complexe fz = f(z);
+  complexe fz = f(z); // valeur courant de la fonction
+  // On itère tant que |f(z)| > ɛ. Pour ne pas avoir à calculer une coûteuse racine carrée, on regarde plutôt |f(z)|^2 < espilon^2.
   epsilon *= epsilon;
-  // on regarde plutôt |f(z)|^2 < espilon^2
   while (std::norm(fz) > epsilon) {
     if (n >= max_it)
-      return { z, -1 };
-    z -= h * fz / f_deriv(z);
+      return { z, -1 }; // pas de convergence
+    z -= fz / f_deriv(z);
     n += 1;
     fz = f(z);
     if (debug)
@@ -34,31 +44,40 @@ std::tuple<complexe,int> methode_newton (
 }
 
 int main () {
+  // Note fonction et sa dérivée
   fonc_complexe_t f = [] (complexe z) { return z*z*z - complexe(1,0); };
   fonc_complexe_t f_deriv = [] (complexe z) { return 3.*z*z; };
+  // Les zéros de notre fonction
   constexpr unsigned int n_zeros = 3;
   complexe zeros [n_zeros] = { {1,0}, {-1/2.,sqrt(3)/2}, {-1/2.,-sqrt(3)/2} };
 
+  /******* Scan de z_0 *******/
+
+  // Définition de la grille
   int N = 1000;
   double a = 1, da = a/N;
 
+  // Fichier stockant le numéro du zéro obtenu
   std::ofstream fichier_bassins_converg ("newton_bassins_converg.txt");
+  // Fichier stockant le nombre d'itérations nécessaires
   std::ofstream fichier_niter ("newton_niter.txt");
 
   for (int i = -N; i <= N; i++) {
     std::cout << i << std::endl;
     for (int j = -N; j <= N; j++) {
 
+      // Méthode de Newton partant de z_0
       complexe z_0 = {i*da, j*da};
-      auto [z_zero, nit] = methode_newton(f, f_deriv, z_0, 40, a*1e-15, 1, false);
+      auto [z_zero, nit] = methode_newton(f, f_deriv, z_0, 40, a*1e-15);
 
-      // détermination du numéro du zéro
+      // Détermination du numéro du zéro obtenu
       int num_zero = -1;
       for (int k = 0; k < n_zeros; k++) {
         if (std::norm(z_zero-zeros[k]) < 1e-10)
           num_zero = k;
       }
 
+      // Écriture dans les fichiers
       fichier_bassins_converg << num_zero << " ";
       fichier_niter << nit << " ";
     }
@@ -66,13 +85,18 @@ int main () {
     fichier_niter << "\n";
   }
 
+  /******* Trajectoire pour un z_0 *******/
+
+  // Fichier stockant une trajectoire
   std::ofstream fichier_traj ("newton_traj.txt");
+  // Fonction callback, écrivant dans le fichier à chaque itération
   std::function<void(complexe)> cb_iter = [&fichier_traj] (complexe z_n) {
     fichier_traj << z_n.real() << " " << z_n.imag() << "\n";
   };
+  // Méthode de Newton partant de z_0, avec enregistrement de la trajectoire
   complexe z_0 = { 0.395, 0.436 };
   cb_iter(z_0);
-  methode_newton(f, f_deriv, z_0, 40, 1e-6, 1, true, cb_iter);
+  methode_newton(f, f_deriv, z_0, 40, 1e-6, true, cb_iter);
 
   return 0;
 }
